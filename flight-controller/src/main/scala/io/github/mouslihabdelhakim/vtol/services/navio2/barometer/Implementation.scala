00601d4a6edbdfd5f67fb8a685e299c90105591f
Abdelhakim Mouslih <mouslih.abdelhakim@gmail.com>
2021-03-06 12:36:09 +0900

    [VTOL-11] switch ms5611 to i2c

--- flight-controller/src/main/scala/io/github/mouslihabdelhakim/vtol/services/navio2/barometer/Implementation.scala
+++ flight-controller/src/main/scala/io/github/mouslihabdelhakim/vtol/services/navio2/barometer/Implementation.scala
@@ -5,7 +5,8 @@ import cats.syntax.functor._
 import cats.syntax.flatMap._
 import cats.effect.{Sync, Timer}
 import com.pi4j.io.i2c.{I2CBus, I2CDevice, I2CFactory}
-import io.github.mouslihabdelhakim.vtol.services.navio2.barometer.Implementation.Reset
+import io.github.mouslihabdelhakim.vtol.services.navio2.barometer.Implementation.{PromReadC1, Reset}
+import io.github.mouslihabdelhakim.vtol.services.navio2.barometer.MS5611.CalibrationData
 
 class Implementation[F[_]](
     i2CDevice: I2CDevice
@@ -18,6 +19,16 @@ class Implementation[F[_]](
     _ <- S.delay(i2CDevice.write(Reset))
     _ <- T.sleep(100.milliseconds)
   } yield ()
+
+  override def promRead(): F[CalibrationData] =
+    read2Bytes(PromReadC1).map(_ => CalibrationData(0, 0, 0, 0, 0, 0))
+
+  private def read2Bytes(register: Int): F[Array[Byte]] = S.delay {
+    val buffer = Array.ofDim[Byte](2)
+    i2CDevice.read(register, buffer, 0, 2)
+    println(buffer.mkString)
+    buffer
+  }
 
 }
 
@@ -34,12 +45,18 @@ object Implementation {
             I2CBus.BUS_1
           )
           .getDevice(
-            0x76 // address
+            0x77 // address https://github.com/emlid/Navio2/blob/0eb90b7d0ace9b88f886f3482c4f275b0a34efe8/Python/navio/ms5611.py#L103
           )
       )
     )
   }
 
-  private val Reset = 0x1e.toByte
-
+  private val Reset      = 0x1e.toByte
+  private val PromReadC1 = 0xa2
+  /*private val PromReadC2 = 0xa4.toByte
+  private val PromReadC3 = 0xa6.toByte
+  private val PromReadC4 = 0xa8.toByte
+  private val PromReadC5 = 0xaa.toByte
+  private val PromReadC6 = 0xac.toByte
+   */
 }
--- flight-controller/src/main/scala/io/github/mouslihabdelhakim/vtol/services/navio2/barometer/MS5611.scala
+++ flight-controller/src/main/scala/io/github/mouslihabdelhakim/vtol/services/navio2/barometer/MS5611.scala
@@ -1,10 +1,13 @@
 package io.github.mouslihabdelhakim.vtol.services.navio2.barometer
 
 import cats.effect.{Sync, Timer}
+import io.github.mouslihabdelhakim.vtol.services.navio2.barometer.MS5611.CalibrationData
 
 trait MS5611[F[_]] {
 
   def reset(): F[Unit]
+
+  def promRead(): F[CalibrationData]
 
 }
 
@@ -14,5 +17,14 @@ object MS5611 {
       S: Sync[F],
       T: Timer[F]
   ): F[MS5611[F]] = Implementation[F]
+
+  case class CalibrationData(
+      C1: Long, // Pressure sensitivity
+      C2: Long, // Pressure offset
+      C3: Long, // Temperature coefficient
+      C4: Long, // Temperature coefficient
+      C5: Long, // Reference temperature
+      C6: Long // Temperature coefficient
+  )
 
 }
