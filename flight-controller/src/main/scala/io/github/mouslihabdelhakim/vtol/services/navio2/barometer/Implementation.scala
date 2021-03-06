0e5fe47a333fba4b660ce62cead681a999ea1495
Abdelhakim Mouslih <mouslih.abdelhakim@gmail.com>
2021-03-06 12:38:20 +0900

    [VTOL-11] switch ms5611 to i2c (#14)
    * switch to i2c
    
    * [VTOL-11] switch ms5611 to i2c

--- flight-controller/src/main/scala/io/github/mouslihabdelhakim/vtol/services/navio2/barometer/Implementation.scala
+++ flight-controller/src/main/scala/io/github/mouslihabdelhakim/vtol/services/navio2/barometer/Implementation.scala
@@ -1,42 +1,62 @@
 package io.github.mouslihabdelhakim.vtol.services.navio2.barometer
 
+import scala.concurrent.duration._
 import cats.syntax.functor._
-
-import cats.effect.Sync
-import com.pi4j.io.spi.{SpiChannel, SpiDevice, SpiFactory}
-import io.github.mouslihabdelhakim.vtol.services.navio2.barometer.Implementation.Command._
+import cats.syntax.flatMap._
+import cats.effect.{Sync, Timer}
+import com.pi4j.io.i2c.{I2CBus, I2CDevice, I2CFactory}
+import io.github.mouslihabdelhakim.vtol.services.navio2.barometer.Implementation.{PromReadC1, Reset}
+import io.github.mouslihabdelhakim.vtol.services.navio2.barometer.MS5611.CalibrationData
 
 class Implementation[F[_]](
-    spiDevice: SpiDevice
+    i2CDevice: I2CDevice
 )(implicit
-    S: Sync[F]
+    S: Sync[F],
+    T: Timer[F]
 ) extends MS5611[F] {
 
-  override def reset(): F[Unit] = S.delay {
-    spiDevice.write(Reset.value: _*)
-  }.void
+  override def reset(): F[Unit] = for {
+    _ <- S.delay(i2CDevice.write(Reset))
+    _ <- T.sleep(100.milliseconds)
+  } yield ()
+
+  override def promRead(): F[CalibrationData] =
+    read2Bytes(PromReadC1).map(_ => CalibrationData(0, 0, 0, 0, 0, 0))
+
+  private def read2Bytes(register: Int): F[Array[Byte]] = S.delay {
+    val buffer = Array.ofDim[Byte](2)
+    i2CDevice.read(register, buffer, 0, 2)
+    println(buffer.mkString)
+    buffer
+  }
 
 }
 
 object Implementation {
 
-  def apply[F[_]](implicit S: Sync[F]): F[MS5611[F]] = {
+  def apply[F[_]](implicit
+      S: Sync[F],
+      T: Timer[F]
+  ): F[MS5611[F]] = {
     S.delay(
       new Implementation[F](
-        SpiFactory.getInstance(
-          SpiChannel.CS0, // https://github.com/emlid/Navio2/blob/0eb90b7d0ace9b88f886f3482c4f275b0a34efe8/Python/navio/ms5611.py#L103
-          SpiDevice.DEFAULT_SPI_SPEED, // https://github.com/emlid/Navio2/blob/0eb90b7d0ace9b88f886f3482c4f275b0a34efe8/Python/navio/ms5611.py#L45
-          SpiDevice.DEFAULT_SPI_MODE // page 6 in https://www.te.com/commerce/DocumentDelivery/DDEController?Action=showdoc&DocId=Data+Sheet%7FMS5611-01BA03%7FB%7Fpdf%7FEnglish%7FENG_DS_MS5611-01BA03_B.pdf%7FCAT-BLPS0036
-        )
+        I2CFactory
+          .getInstance(
+            I2CBus.BUS_1
+          )
+          .getDevice(
+            0x77 // address https://github.com/emlid/Navio2/blob/0eb90b7d0ace9b88f886f3482c4f275b0a34efe8/Python/navio/ms5611.py#L103
+          )
       )
     )
   }
 
-  sealed abstract class Command(val value: Array[Byte])
-
-  object Command {
-
-    case object Reset extends Command(Array(0x1e, 0x00, 0x00, 0x00))
-  }
-
+  private val Reset      = 0x1e.toByte
+  private val PromReadC1 = 0xa2
+  /*private val PromReadC2 = 0xa4.toByte
+  private val PromReadC3 = 0xa6.toByte
+  private val PromReadC4 = 0xa8.toByte
+  private val PromReadC5 = 0xaa.toByte
+  private val PromReadC6 = 0xac.toByte
+   */
 }
