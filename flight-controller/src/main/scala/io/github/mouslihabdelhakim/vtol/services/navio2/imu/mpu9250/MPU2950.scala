f12eead665b0609cbebaef7a338d0a0460e31f62
Abdelhakim Mouslih <mouslih.abdelhakim@gmail.com>
2021-03-15 23:11:51 +0900

    [VTOL-22] replace reset and testConnection with init (#25)

--- flight-controller/src/main/scala/io/github/mouslihabdelhakim/vtol/services/navio2/imu/mpu9250/MPU2950.scala
+++ flight-controller/src/main/scala/io/github/mouslihabdelhakim/vtol/services/navio2/imu/mpu9250/MPU2950.scala
@@ -1,19 +1,18 @@
 package io.github.mouslihabdelhakim.vtol.services.navio2.imu.mpu9250
 
-import cats.effect.Sync
+import cats.effect.{Sync, Timer}
 
 trait MPU2950[F[_]] {
 
-  def testConnection(): F[Boolean]
-
-  def reset(): F[Unit]
+  def init(): F[Unit]
 
 }
 
 object MPU2950 {
 
   def spi[F[_]](implicit
-      S: Sync[F]
+      S: Sync[F],
+      T: Timer[F]
   ): F[MPU2950[F]] = SPIBasedImplementation[F]
 
 }
--- flight-controller/src/main/scala/io/github/mouslihabdelhakim/vtol/services/navio2/imu/mpu9250/SPIBasedImplementation.scala
+++ flight-controller/src/main/scala/io/github/mouslihabdelhakim/vtol/services/navio2/imu/mpu9250/SPIBasedImplementation.scala
@@ -1,30 +1,59 @@
 package io.github.mouslihabdelhakim.vtol.services.navio2.imu.mpu9250
 
+import cats.effect.{Sync, Timer}
+import cats.syntax.flatMap._
 import cats.syntax.functor._
-import cats.effect.Sync
 import com.pi4j.io.spi.{SpiChannel, SpiDevice, SpiFactory}
+import io.github.mouslihabdelhakim.vtol.services.navio2.imu.mpu9250.SPIBasedImplementation.Register._
+
+import scala.concurrent.duration._
 
 class SPIBasedImplementation[F[_]](
     spiDevice: SpiDevice
 )(implicit
-    S: Sync[F]
+    S: Sync[F],
+    T: Timer[F]
 ) extends MPU2950[F] {
   import SPIBasedImplementation._
 
-  override def testConnection(): F[Boolean] = S.delay {
-    spiDevice.write(ReadWhoAmI, Filler)(1) == ExpectedWhoAmIResponse
+  override def init(): F[Unit] = for {
+    _ <- testMPU2950Connection
+    _ <- initMPU2950()
+  } yield ()
+
+  private def initMPU2950(): F[Unit] = for {
+    _ <- slowWrite(PWR_MGMT_1.DEVICE_RESET)
+    _ <- slowWrite(PWR_MGMT_1.CLKSEL)
+    _ <- slowWrite(CONFIG.DLPF_CFG)
+    _ <- slowWrite(SMPRT_DIV.SMPLRT_DIV)
+    _ <- slowWrite(GYRO_CONFIG.FS_SEL)
+    _ <- slowWrite(ACCEL_CONFIG.AFS_SEL)
+  } yield ()
+
+  private def testMPU2950Connection: F[Unit] = readByte(WHO_AM_I).map {
+    case WHO_AM_I.ExpectedValue => ()
+    case other                  => throw new Exception(s"expected ${WHO_AM_I.ExpectedValue}, but received ${other}")
   }
 
-  override def reset(): F[Unit] = S.delay {
-    spiDevice.write(PwrMgmt1, PwrMgmt1DeviceReset)
-  }.void
+  private def slowWrite(parameter: Parameter): F[Unit] =
+    for {
+      _ <- S.delay {
+             spiDevice.write(parameter.address, parameter.value)
+           }
+      _ <- T.sleep(50.milliseconds)
+    } yield ()
+
+  private def readByte(register: Register): F[Byte] = S.delay {
+    spiDevice.write(register.READ, 0x00.toByte)(1)
+  }
 
 }
 
 object SPIBasedImplementation {
 
   def apply[F[_]](implicit
-      S: Sync[F]
+      S: Sync[F],
+      T: Timer[F]
   ): F[MPU2950[F]] =
     S.delay {
       SpiFactory.getInstance(
@@ -35,18 +64,43 @@ object SPIBasedImplementation {
 
   private val SPISpeed = 20000000 // 20Mhz
 
-  // commands
-  private val PwrMgmt1DeviceReset = 0x80.toByte
+  abstract class Register(protected val address: Byte) {
+    import Register._
+    val READ: Byte = (address | ReadFlag).toByte
+  }
 
-  // Reading
-  private val ReadFlag: Byte = 0x80.toByte
-  private val Filler: Byte   = 0x00
+  object Register {
+    val ReadFlag: Byte = 0x80.toByte
 
-  // registers
-  private val PwrMgmt1: Byte   = 0x6b.toByte
-  private val ReadWhoAmI: Byte = (0x75 | ReadFlag).toByte
+    case class Parameter(address: Byte, value: Byte)
 
-  // expected response
-  private val ExpectedWhoAmIResponse = 0x71.toByte
+    case object PWR_MGMT_1 extends Register(address = 0x6b.toByte) {
+      val DEVICE_RESET = Parameter(address, 0x80.toByte) // resets all internal registers to their default values.
+      val CLKSEL       = Parameter(address, 0x01.toByte) // Specifies the X axis gyroscope as the clock source of the device
+    }
+
+    case object CONFIG extends Register(address = 0x1a.toByte) {
+      val DLPF_CFG = Parameter(address, 0x04.toByte) // enable the Digital Low Pass Filter
+    }
+
+    case object SMPRT_DIV extends Register(address = 0x19.toByte) {
+      val SMPLRT_DIV = Parameter(address, 0x01.toByte) // keep the sample rate at 1kHz
+    }
+
+    case object GYRO_CONFIG extends Register(address = 0x1b.toByte) {
+      val FS_SEL = Parameter(address, 0x18.toByte) // set the gyroscope full scale range to ±2000°/s
+    }
+
+    case object ACCEL_CONFIG extends Register(address = 0x1c.toByte) {
+      val AFS_SEL = Parameter(address, 0x3.toByte) // set the full scale range of the accelerometer to ±16g
+    }
+
+    case object WHO_AM_I extends Register(address = 0x75.toByte) {
+
+      val ExpectedValue = 0x71.toByte
+
+    }
+
+  }
 
 }
