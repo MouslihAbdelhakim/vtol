ebd8744dc958f5400f6df07f75f5b44e18aec143
Abdelhakim Mouslih <mouslih.abdelhakim@gmail.com>
2021-03-16 23:44:03 +0900

    stream

--- flight-controller/src/main/scala/io/github/mouslihabdelhakim/vtol/services/navio2/imu/mpu9250/MPU2950.scala
+++ flight-controller/src/main/scala/io/github/mouslihabdelhakim/vtol/services/navio2/imu/mpu9250/MPU2950.scala
@@ -3,8 +3,11 @@ package io.github.mouslihabdelhakim.vtol.services.navio2.imu.mpu9250
 import cats.Show
 import cats.effect.{Sync, Timer}
 import cats.syntax.show._
-import io.github.mouslihabdelhakim.vtol.services.navio2.imu.mpu9250.MPU2950.{CalibrationData, ImuData}
+import fs2.Stream
 import io.github.mouslihabdelhakim.vtol.services.navio2.imu.mpu9250.MPU2950.ImuData.{Accelerations, AngularRates}
+import io.github.mouslihabdelhakim.vtol.services.navio2.imu.mpu9250.MPU2950.{CalibrationData, ImuData}
+
+import scala.concurrent.duration.FiniteDuration
 
 trait MPU2950[F[_]] {
 
@@ -20,6 +23,21 @@ object MPU2950 {
       S: Sync[F],
       T: Timer[F]
   ): F[MPU2950[F]] = SPIBasedImplementation[F]
+
+  def stream[F[_]](
+      sampleEvery: FiniteDuration
+  )(implicit
+      S: Sync[F],
+      T: Timer[F]
+  ): Stream[F, ImuData] = for {
+    imu <- Stream.eval(spi[F])
+    _ <- Stream.eval(imu.init())
+    data <- Stream
+              .repeatEval(
+                imu.read(SPIBasedImplementation.preSetCalibration)
+              )
+              .metered(sampleEvery)
+  } yield data
 
   case class CalibrationData(
       accelerationDivider: Double,
@@ -40,9 +58,9 @@ object MPU2950 {
 
     object Accelerations {
       implicit val show: Show[Accelerations] = Show.show { acc =>
-        val x = s"x: ${acc.xAxisInMeterPerSecondPerSecond}%2.2f m/s²"
-        val y = s"y: ${acc.yAxisInMeterPerSecondPerSecond}%2.2f m/s²"
-        val z = s"z: ${acc.zAxisInMeterPerSecondPerSecond}%2.2f m/s²"
+        val x = f"x: ${acc.xAxisInMeterPerSecondPerSecond}%2.2f m/s²"
+        val y = f"y: ${acc.yAxisInMeterPerSecondPerSecond}%2.2f m/s²"
+        val z = f"z: ${acc.zAxisInMeterPerSecondPerSecond}%2.2f m/s²"
         s"Accelerations(${x}, ${y}, ${z})"
       }
     }
@@ -55,10 +73,10 @@ object MPU2950 {
 
     object AngularRates {
       implicit val show: Show[AngularRates] = Show.show { ang =>
-        val pitch = s"x: ${ang.pitchAxisInRadPerSecond}%2.2f rad/s"
-        val yaw   = s"y: ${ang.yawAxisInRadPerSecond}%2.2f rad/s"
-        val roll  = s"z: ${ang.rollAxisInRadPerSecond}%2.2f rad/s"
-        s"Accelerations(pitch: ${pitch}, yaw: ${yaw}, roll: ${roll})"
+        val pitch = f"pitch: ${ang.pitchAxisInRadPerSecond}%2.2f rad/s"
+        val yaw   = f"yaw: ${ang.yawAxisInRadPerSecond}%2.2f rad/s"
+        val roll  = f"roll: ${ang.rollAxisInRadPerSecond}%2.2f rad/s"
+        s"Accelerations(${pitch}, ${yaw}, ${roll})"
       }
     }
 
